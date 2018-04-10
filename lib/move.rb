2363e118bdb811454a93f572ea2ce0fd0cfe53bf
Joyce Chun <wooyangee@gmail.com>
2018-04-10 19:33:26 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(input=["1", "2", "3", "4", "5", "6", "7", "8","9"])
   index
 end
 
-def move(boardwithmove, index, char="X")
-  boardwithmove[index] = char
+def move(boardwithx, index, char="X")
+  boardwithx[index] = char
 end
 
